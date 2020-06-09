ec8eba36473c67ce821175a0b826bb099fb8dc22
bhoomikadasar60 <66351262+bhoomikadasar60@users.noreply.github.com>
2020-06-09 15:25:08 +0530

    created switchcase

---
+++ SwitchcaseJava.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package switchcase.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author bhoomika dasar
+ */
+public class SwitchcaseJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int choice;
+    System.out.println("Pick one : 1. Hi\t2. Hey\t3. hello\t");
+    Scanner s = new Scanner(System.in);
+    choice = s.nextInt();
+    switch(choice)
+    {
+        case 1 : System.out.println("You said Hi");
+                break;
+        case 2 : System.out.println("You said Hey");
+                break;
+        case 3 : System.out.println("You said Hello");
+                break;
+        default : System.out.println("Invalid choice.");
+    }        
+    }
+    
+}   
+           
+    
+    
+    
+}
